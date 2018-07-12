e9409051511c6af2b4c06c2358572537a59aebf9
gavin-schaeferle <41128634+gavin-schaeferle@users.noreply.github.com>
2018-07-12 11:06:05 -0500

    back to the past

--- welcome.py
+++ welcome.py
@@ -19,8 +19,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def Welcome():
-     return "Hello World"
-    #return app.send_static_file('index.html')
+     #return "Hello World"
+    return app.send_static_file('index.html')
 
 @app.route('/myapp')
 def WelcomeToMyapp():
